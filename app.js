2621302c9f095ff7dced352e08026a4b008b2749
harifa123 <harifarifu@gmail.com>
2024-02-08 15:47:19 +0530

    search

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express=require("express")
 const cors=require("cors")
-const mongoos=require("mongoose")
+const mongoose=require("mongoose")
 const patientRoute=require("./controller/patientRouter")
-const { default: mongoose } = require("mongoose")
 
 const app=express()
 
